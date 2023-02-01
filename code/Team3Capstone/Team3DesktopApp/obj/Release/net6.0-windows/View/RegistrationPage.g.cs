fad32d8d2ff9099d0d2b330981d21ff3416705f0
csuser@cs.westga.edu <csuser@cs.westga.edu>
2023-02-01 14:02:40 -0500

    Adding existing project files to main solution
    > Adding RecipePlannerApi and RecipePlanner DB to Team3Capstone solution
    to enabling referencing

--- code/Team3Capstone/Team3DesktopApp/obj/Release/net6.0-windows/View/RegistrationPage.g.cs
+++ code/Team3Capstone/Team3DesktopApp/obj/Release/net6.0-windows/View/RegistrationPage.g.cs
@@ -126,7 +126,7 @@ namespace Team3 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -142,7 +142,7 @@ namespace Team3 {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.1.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
