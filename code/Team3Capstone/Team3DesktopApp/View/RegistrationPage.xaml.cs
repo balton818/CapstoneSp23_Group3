78ee465fbdfb8da138bd768e57e962eff243d1b3
jwagner2 <89319875+jwagner2@users.noreply.github.com>
2023-03-04 19:03:49 -0500

    Change registration to be single page
    Changed registration to be on single page.
    Added additional error checking and email regex

--- code/Team3Capstone/Team3DesktopApp/View/RegistrationPage.xaml.cs
+++ code/Team3Capstone/Team3DesktopApp/View/RegistrationPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using Team3DesktopApp.ViewModel;
@@ -39,8 +40,13 @@ public sealed partial class RegistrationPage : Page
             !string.IsNullOrWhiteSpace(this.pwTextBox.Text) &&
             !string.IsNullOrWhiteSpace(this.emailTextBox.Text) &&
             !string.IsNullOrWhiteSpace(this.firstNameTextBox.Text) &&
-            !string.IsNullOrWhiteSpace(this.lastNameTextBox.Text))
+            !string.IsNullOrWhiteSpace(this.lastNameTextBox.Text) &&
+            !string.IsNullOrWhiteSpace(this.pwConfirmBox.Text))
         {
+            if (!this.errorChecking())
+            {
+                return;
+            }
             var result = await ((this.ViewModel != null
                 ? this.ViewModel.RegisterUser(this.unTextBox.Text, this.pwTextBox.Text,
                     this.emailTextBox.Text,
@@ -48,8 +54,8 @@ public sealed partial class RegistrationPage : Page
                 : null)!);
             if (result < 0)
             {
-                this.errorLabel.Text = "error registering user";
-                this.errorLabel.Visibility = Visibility.Visible;
+                this.generalError.Text = "error registering user";
+                this.generalError.Visibility = Visibility.Visible;
             }
             else
             {
@@ -62,8 +68,62 @@ public sealed partial class RegistrationPage : Page
         }
         else
         {
-            this.errorLabel.Visibility = Visibility.Visible;
+            this.generalError.Visibility = Visibility.Visible;
         }
+    }
+
+    private bool errorChecking()
+    {
+        var errors = 0;
+        this.unErrorLabel.Visibility = Visibility.Collapsed;
+        this.pwError.Visibility = Visibility.Collapsed;
+        this.emailError.Visibility = Visibility.Collapsed;
+        this.nameError.Visibility = Visibility.Collapsed;
+        Regex emailPattern = new Regex("^'\'w +@[a - zA - Z_] +?'\'.[a-zA - Z]{ 2,3}$",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        if (!this.pwTextBox.Text.Equals(this.pwConfirmBox.Text))
+        {
+            this.pwError.Text = "Passwords do not match";
+            this.pwError.Visibility = Visibility.Visible;
+            errors++;
+
+        }
+
+        if (string.IsNullOrEmpty(this.pwTextBox.Text) || string.IsNullOrEmpty(this.pwConfirmBox.Text))
+        {
+            this.pwError.Text = "Password Cannot be empty";
+            this.pwError.Visibility = Visibility.Visible;
+            errors++;
+        }
+        if (String.IsNullOrEmpty(this.unTextBox.Text))
+        {
+            this.unErrorLabel.Visibility = Visibility.Visible;
+            errors++;
+        }
+        if (string.IsNullOrEmpty(this.firstNameTextBox.Text) || string.IsNullOrEmpty(this.lastNameTextBox.Text))
+        {
+            this.nameError.Visibility = Visibility.Visible;
+            this.nameError.Text = "Name fields cannot be empty";
+            errors++;
+        }
+        if (string.IsNullOrEmpty(this.emailTextBox.Text))
+        {
+            this.emailError.Visibility = Visibility.Visible;
+            this.emailError.Text = "Email cannot be empty";
+            errors++;
+        }
+        if (!emailPattern.IsMatch(this.emailTextBox.Text))
+        {
+            this.emailError.Visibility = Visibility.Visible;
+            this.emailError.Text = "Email is not valid";
+            errors++;
+        }
+
+        if (errors > 0)
+        {
+            return false;
+        }
+        return true;
     }
 
     /// <summary>Handles the Click event of the BackButton_OnClickButton control.</summary>
@@ -82,8 +142,10 @@ public sealed partial class RegistrationPage : Page
 
     private void SubmitButton_OnClick_(object sender, RoutedEventArgs e)
     {
-        this.errorLabel.Visibility = Visibility.Collapsed;
+        this.unErrorLabel.Visibility = Visibility.Collapsed;
         this.pwError.Visibility = Visibility.Collapsed;
+        this.emailError.Visibility = Visibility.Collapsed;
+        this.nameError.Visibility = Visibility.Collapsed;
         if (String.IsNullOrEmpty(this.unTextBox.Text))
         {
             this.errorLabel.Visibility = Visibility.Visible;
