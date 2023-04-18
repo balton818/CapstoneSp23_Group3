d448212645dfc2bc168ed7bf6e91fabfaf11a1a7
balton818 <balton1@my.westga.edu>
2023-04-18 19:26:03 -0400

    Added granular input validation
    > Modified current validation scheme to respond to specific input errors.
    > Modified page navigation flow.

--- code/Team3Capstone/RecipePlannerWebApp/LocalServices/UserSessionData.cs
+++ code/Team3Capstone/RecipePlannerWebApp/LocalServices/UserSessionData.cs
@@ -6,17 +6,13 @@ namespace RecipePlannerWebApp.LocalServices
     public class UserSessionData
     {
         public User CurrentUser { get; set; } = new User();
-        
         public string? CurrentRecipeTitle { get; set; }
-
         public BrowseRecipeRequest? lastExplorePageRequest { get; set; }
-
         public Meal? SelectedMeal { get; set; }
         public bool NewMeal { get; set; }
         public bool UpdateMeal { get; set; }
         public Weeks SelectedWeek { get; set; } = Weeks.WEEK1;
         public enum Weeks { WEEK1, WEEK2 };
-        public Dictionary<ShoppingListIngredient, bool> userShoppingSelection { get; set; } = new Dictionary<ShoppingListIngredient, bool>();
         public Dictionary<int, bool> userSelectedIDs { get; set; } = new Dictionary<int, bool>();
     }
 }
